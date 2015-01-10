ee57ad216555a06abc8da4f4da74bc98a62f254f
Benjamin Titcomb <benjamin@benjamint>
2015-01-09 18:07:42 -0800

    First attempt at making methods chainable.

--- opal/async/enumerator.rb
+++ opal/async/enumerator.rb
@@ -1,7 +1,22 @@
 class Enumerator
   def initialize enumerable
     @enumerable = enumerable
+    @jobs = []
+    @jobs_finished = 0
+    @jobs_queued = 0
     set_defaults
+  end
+  def queue proc
+    @jobs << proc
+    @jobs_queued += 1
+    watcher = Interval.new 0 do
+      if @jobs.count == 1
+        @jobs.first.call
+        watcher.stop
+      elsif @jobs.count.zero?
+        watcher.stop
+      end
+    end
   end
   def set_defaults
     @output = []
@@ -9,47 +24,59 @@ class Enumerator
     @length = @enumerable.length
   end
   def each step=1, &block
-    set_defaults
-    @step = step
-    Countdown.new 0, @length, @step do |cdown, ind|
-      Timeout.new 0 do
-        yield(@enumerable[ind], ind)
-        @finished = true if cdown <= 1
+    proc = Proc.new do
+      set_defaults
+      @step = step
+      Countdown.new 0, @length, @step do |cdown, ind|
+        Timeout.new 0 do
+          yield(@enumerable[ind], ind)
+          # @finished = true if cdown <= 1
+          finish_job if cdown <= 1
+        end
       end
+      @output = @enumerable
     end
-    @output = @enumerable
+    queue proc
     self
   end
   def each_slice step=1, &block
-    set_defaults
-    @step = step
-    chunk = []
-    block_was_given = block_given?
-    Countdown.new 0, @length, 1 do |cdown, ind|
-      Timeout.new 0 do
-        chunk << @enumerable[ind]
-        if (ind + 1) % @step == 0
-          if block_was_given
-            @output << yield(@enumerable[ind], ind)
-          else
-            @output << chunk
+    proc = Proc.new do
+      set_defaults
+      @step = step
+      chunk = []
+      block_was_given = block_given?
+      Countdown.new 0, @length, 1 do |cdown, ind|
+        Timeout.new 0 do
+          chunk << @enumerable[ind]
+          if (ind + 1) % @step == 0
+            if block_was_given
+              @output << yield(@enumerable[ind], ind)
+            else
+              @output << chunk
+            end
+            chunk = []
           end
-          chunk = []
+          # @finished = true if cdown <= 1
+          finish_job if cdown <= 1
         end
-        @finished = true if cdown <= 1
       end
     end
+    queue proc
     self
   end
   def map step=1, &block
-    set_defaults
-    @step = step
-    Countdown.new 0, @length, @step do |cdown, ind|
-      Timeout.new 0 do
-        @output << yield(@enumerable[ind], ind)
-        @finished = true if cdown <= 1
+    proc = Proc.new do
+      set_defaults
+      @step = step
+      Countdown.new 0, @length, @step do |cdown, ind|
+        Timeout.new 0 do
+          @output << yield(@enumerable[ind], ind)
+          # @finished = true if cdown <= 1
+          finish_job if cdown <= 1
+        end
       end
     end
+    queue proc
     self
   end
   def done &block
@@ -58,13 +85,17 @@ class Enumerator
   def then &block
     complete Proc.new {yield(self.class.new(@output))}
   end
+  def finish_job
+    @jobs.shift
+    @jobs_finished += 1
+  end
   def complete proc
     checker = Interval.new 0 do
-      if @finished
+      if (@jobs_finished >= @jobs_queued)
         checker.stop
-        @finished = false
         proc.call
       end
-    end      
+    end
+    self
   end
 end
